15f0a467986572cfdab001b6e6fa52c1c77ebf74
NEFEDOV_IT <eliaz_one@mail.ru>
2022-02-17 23:16:17 +0300

    add validate, sendEmail, error style

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-import {UI_ELEMENTS} from './view.js'
+import { UI_ELEMENTS, API } from './view.js'
 import { popup } from './popup.js'
+
+const state = {
+    ERROR_EMAIL: 'Введите корректный e-mail',
+    ERROR_DATA: "Запрос не удался",
+}
+
+popup()
 
 UI_ELEMENTS.formChat.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -8,14 +15,22 @@ UI_ELEMENTS.formChat.addEventListener('submit', (e) => {
 })
 
 function showMessage(message) {
-    UI_ELEMENTS.windowChat.prepend(UI_ELEMENTS.template.content.cloneNode(true))
-    UI_ELEMENTS.windowChat.querySelector('.chat__text').textContent = `Я: ${message}`
-    UI_ELEMENTS.windowChat.querySelector('.chat__time').textContent = timeConverter(new Date())
+    if (!message.trim()) return UI_ELEMENTS.formChat.reset()
+
+    const textChat = UI_ELEMENTS.template.content.querySelector('.chat__my-message .chat__text')
+    const timeChatMessage = UI_ELEMENTS.template.content.querySelector('.chat__my-message .chat__time')
+
+    textChat.textContent = `Я: ${message}`
+    timeChatMessage.textContent = timeConverter()
+
+    const sendMessage = UI_ELEMENTS.template.content.cloneNode(true)
+    UI_ELEMENTS.windowChat.prepend(sendMessage)
+
     UI_ELEMENTS.formChat.reset()
 }
 
-function timeConverter(data) {
-    const TIME_DATA = new Date(data)
+function timeConverter() {
+    const TIME_DATA = new Date()
     let hour = TIME_DATA.getHours()
     let min = TIME_DATA.getMinutes()
     min = (min < 10) ? '0' + min : min
@@ -23,4 +38,54 @@ function timeConverter(data) {
     return hour + ':' + min
 }
 
-popup()
+UI_ELEMENTS.authorizationForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+UI_ELEMENTS.authorizationButton.addEventListener('click', () => {
+    const mailAddress = UI_ELEMENTS.authorizationInput.value
+    validateEmail(mailAddress)
+})
+
+function validateEmail(address) {
+    const reg = /^([A-Za-z\d_\-.])+@([A-Za-z\d_\-.])+\.([A-Za-z]{2,4})$/
+    if (!reg.test(address)) {
+        UI_ELEMENTS.authorizationForm.reset()
+        UI_ELEMENTS.authorizationInput.classList.add('error')
+        return alert(state.ERROR_EMAIL)
+    } else sendEmail(address)
+}
+
+UI_ELEMENTS.verificationButton.addEventListener('click', () => {
+    UI_ELEMENTS.verification.classList.remove('open')
+})
+
+function sendEmail(mailAddress) {
+    const isValid = UI_ELEMENTS.authorizationInput.classList.contains('error')
+    const json = JSON.stringify({email: mailAddress})
+
+    if (isValid) {
+        UI_ELEMENTS.authorizationForm.reset()
+        UI_ELEMENTS.authorizationInput.classList.remove('error')
+    }
+
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', API.URL)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+    xhr.send(json)
+
+    xhr.onload = function () {
+        if (xhr.status !== 200) {
+            alert(`Ошибка ${xhr.status}`)
+        }
+
+        UI_ELEMENTS.authorizationForm.reset()
+        UI_ELEMENTS.authorization.classList.remove('open')
+        UI_ELEMENTS.verification.classList.add('open')
+        alert(`Готово, получили ${xhr.response.length} байт`)
+    }
+
+    xhr.onerror = function () {
+        alert(state.ERROR_DATA)
+    }
+}
--- view.js
+++ view.js
@@ -2,5 +2,15 @@ export const UI_ELEMENTS = {
     formChat: document.querySelector('.chat__form'),
     windowChat: document.querySelector('.chat__window'),
     template: document.querySelector('.template'),
+    authorizationForm: document.querySelector('.popup__authorization-form'),
+    authorization: document.querySelector('#popup__authorization'),
+    authorizationInput: document.querySelector('#authorization'),
+    authorizationButton: document.querySelector('.popup__authorization-button'),
+    verificationButton: document.querySelector('.popup__verification-button'),
+    verification: document.querySelector('#popup__verification'),
+}
+
+export const API = {
+    URL: 'https://chat1-341409.oa.r.appspot.com/api/user',
 }
 
